100dbab33809456aef8cf67c0431f5cc8e0c0c74
Abdurakhman <abdibaitovabdurahman@gmail.com>
2024-10-29 19:29:17 +0600

    fixed endpoint on get caitem

--- src/app/api/v1/get-caitem/route.ts
+++ src/app/api/v1/get-caitem/route.ts
@@ -1,14 +1,16 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
 
 export const GET = async () => {
     try {
         const data = await prisma.caitem.findMany();
-        return NextResponse.json(data)
+        return NextResponse.json(data);
     } catch (error) {
-        return NextResponse.json(error)
+        console.error("Error fetching data:", error); 
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    } finally {
+        await prisma.$disconnect(); 
     }
-}
+};

62f5d05ef26ac420feca051fb73eca43de6ef001
Abdurakhman <abdibaitovabdurahman@gmail.com>
2024-11-07 16:24:32 +0600

    fixed favorite block

--- src/components/pages/itemDetails/ItemDetails.tsx
+++ src/components/pages/itemDetails/ItemDetails.tsx
@@ -187,7 +187,7 @@ const ItemDetails = () => {
                                   />{" "}
                                 </>
                               ) : (
-                                <>
+                                <> 
                                   {" "}
                                   <FavoriteIcon
                                     sx={{
